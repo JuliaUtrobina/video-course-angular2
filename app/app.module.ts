5a715935c80bdf3aeb02187c24c2b791e899ce79
Julia <utrobinajulia@gmail.com>
2018-07-13 22:05:21 +0300

    Created project, Tried new Template Syntax, Created Reusable Services Added routing

---
+++ app/app.module.ts
@@ -0,0 +1,50 @@
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+import {RouterModule} from '@angular/router'
+
+import {EventsAppComponent} from "./events-app.components";
+import {EventsListComponent} from "./events/events-list.component";
+import {EventThumbnailComponent} from  "./events/events-thumbnail.component";
+import {NavBarComponent} from  "./nav/navbar.component";
+import {EventService} from "./events/shared/event.service"
+import {ToastrService} from "./common/toastr.service"
+import {EventDetailsComponent} from "./events/event-details/event-details.component"
+import {appRoutes} from "./routes";
+import {CreateEventComponent} from './events/create-event.component'
+import {Error404Component} from './errors/404.component'
+import {EventRouteActivator} from './events/event-details/event-route-activator.service'
+import {EventListResolver} from "./events/events-list-resolver.service";
+
+
+
+@NgModule({
+    imports:[
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    declarations:[
+        EventsAppComponent,
+        EventsListComponent,
+        EventThumbnailComponent,
+        EventDetailsComponent,
+        NavBarComponent,
+        CreateEventComponent,
+        Error404Component
+    ],
+    providers:[
+        EventService,
+        ToastrService,
+        EventListResolver,
+        EventRouteActivator,
+        {
+            provide:'canDeactivateCreateEvent',
+            useValue:checkDirtyState
+        }
+    ],
+    bootstrap:[EventsAppComponent]
+})
+export class AppModule{}
+
+function checkDirtyState(){
+    return false;
+}
---
+++ app/events/events-list.component.ts
@@ -0,0 +1,35 @@
+import {Component} from '@angular/core';
+import {EventService} from "./shared/event.service";
+import {OnInit} from "../../node_modules/@angular/core/src/metadata/lifecycle_hooks";
+import {ToastrService} from "../common/toastr.service";
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+    template:`
+    <div>
+        <h1>Upcoming Angular 2 Events</h1>
+        <hr/>
+        <div class="row">
+            <div *ngFor="let event of events" class="col-md-5">
+                <events-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></events-thumbnail>
+            </div>
+        </div>
+    </div>
+    `
+})
+export class EventsListComponent implements OnInit{
+    events:any[];
+
+
+    constructor (private eventService:EventService, private toastr: ToastrService, private route:ActivatedRoute){
+
+    }
+
+    ngOnInit(){
+        this.events=this.route.snapshot.data['events'];
+    }
+
+    handleThumbnailClick(eventName){
+        this.toastr.success(eventName);
+    }
+}
