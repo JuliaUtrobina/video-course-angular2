38c0f216f7e3d59d26a7fe33fa8396e00475e25b
Julia <utrobinajulia@gmail.com>
2018-07-24 20:53:54 +0300

    Added build-in pipes for name, date and currency

--- app/events/events-thumbnail.component.ts
+++ app/events/events-thumbnail.component.ts
@@ -2,18 +2,18 @@ import {Component, Input} from '@angular/core';
 import {IEvent} from "./shared/index";
 
 @Component({
-    selector:'events-thumbnail',
-    template:`
+    selector: 'events-thumbnail',
+    template: `
     <div [routerLink]="['/events',event.id]" class="well hoverwell thumbnail">
-        <h2>{{event?.name}}</h2>
-        <div>Date: {{event?.date}}</div>
+        <h2>{{event?.name | uppercase}}</h2>
+        <div>Date: {{event?.date |date:'shortDate'}}</div>
         <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
             Time: {{event?.time}}
             <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
             <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
             <span *ngSwitchDefault>(Normal Start)</span>
         </div>
-        <div>Price: \${{event?.price}}</div>
+        <div>Price: {{event?.price | currency:'USD':true}}</div>
         <div *ngIf="event?.location">
             <span>Location: {{event?.location?.address}}</span>
             <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
@@ -23,18 +23,18 @@ import {IEvent} from "./shared/index";
         </div>
      </div>
     `,
-    styles:[`
+    styles: [`
         .thumbnail {min-height: 210px;}
         .pad-left{margin-left:10px;}
         .well div {color: #bbb}
     `]
 })
-export class EventThumbnailComponent{
+export class EventThumbnailComponent {
     @Input() event:IEvent
 
-    getStartTimeStyle():any{
-        if(this.event && this.event.time==='8:00 am')
-            return {color:'#003300','font-weight':'bold'}
+    getStartTimeStyle():any {
+        if (this.event && this.event.time === '8:00 am')
+            return {color: '#003300', 'font-weight': 'bold'}
         return {}
     }
 }
