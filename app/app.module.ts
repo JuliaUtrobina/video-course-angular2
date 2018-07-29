60c0587ef3f65aca1449f4c89a2d91e048d0e89a
Julia <utrobinajulia@gmail.com>
2018-07-29 21:34:07 +0300

    Fixed the issue with routing, It was used

--- app/app.module.ts
+++ app/app.module.ts
@@ -81,6 +81,8 @@ declare let jQuery:Object;
 export class AppModule {
 }
 
-function checkDirtyState() {
-    return false;
+export function checkDirtyState(component:CreateEventComponent) {
+    if (component.isDirty)
+        return window.confirm('You have not saved this event, do you really want to cancel?')
+    return true
 }
