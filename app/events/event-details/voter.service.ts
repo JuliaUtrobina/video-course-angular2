2ed6ad5fc1a29533409fb416bdf99bb7b138edf7
Julia <utrobinajulia@gmail.com>
2018-08-05 14:57:16 +0300

    Added http requests to the VotesService. Tried to use delete method

--- app/events/event-details/voter.service.ts
+++ app/events/event-details/voter.service.ts
@@ -1,18 +1,36 @@
 import {Injectable} from "@angular/core";
-import {ISession} from "../shared/event.model";
+import {IEvent, ISession} from "../shared/event.model";
+import {Http, Response, Headers, RequestOptions} from "@angular/http";
+import {Observable} from "rxjs/Rx";
 
 @Injectable()
 
 export class VoterService {
-    deleteVoter(session: ISession, voterName: string) {
-        session.voters = session.voters.filter(voter => voter !== voterName);
+    constructor(private http: Http) {
+
     }
 
-    addVoter(session: ISession, voterName: string) {
+    deleteVoter(eventId: number, session: ISession, voterName: string) {
+        session.voters = session.voters.filter(voter => voter !== voterName);
+
+        let ulr = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+        return this.http.delete(ulr).catch(this.handleError).subscribe();
+    }
+
+    addVoter(eventId: number, session: ISession, voterName: string) {
         session.voters.push(voterName);
+
+        let headers = new Headers({'Content-Type': 'application/json'});
+        let options = new RequestOptions({headers: headers});
+        let ulr = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+        return this.http.post(ulr, JSON.stringify({}), options).catch(this.handleError).subscribe();
     }
 
     userHasVoted(session: ISession, voterName: string) {
         return session.voters.some(voter => voter === voterName);
+    }
+
+    private handleError(error: Response) {
+        return Observable.throw(error.statusText);
     }
 }
